b0462bb4530026050fbd9421fbe345b72410010f
Felix Abbott <felix.abbott@mymail.champlain.edu>
2022-02-10 14:15:35 -0500

    cimplement steered cohesion

---
+++ Flocking Algorithm 2D/Assets/Scripts/Behavior Scripts/SteeredCohesionBehavior.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Steered Cohesion")]
+public class SteeredCohesionBehavior : FlockBehavior
+{
+    // Variables
+    private Vector2 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        //if no neighbors return no adjustment
+        if (context.Count == 0)
+        {
+            return Vector2.zero;
+        }
+
+        // add all points togehter and average
+        Vector2 cohesionMove = Vector2.zero;
+        foreach (Transform item in context)
+        {
+            cohesionMove += (Vector2)item.position;
+        }
+        cohesionMove /= context.Count;
+
+        // create offset from agent position
+        cohesionMove -= (Vector2)agent.transform.position;
+        cohesionMove = Vector2.SmoothDamp(agent.transform.up, cohesionMove, ref currentVelocity, agentSmoothTime);
+        // Passed in as a ref so the original will have it's value changed
+        return cohesionMove;
+    }
+}
